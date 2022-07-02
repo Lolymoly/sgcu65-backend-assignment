4f3127678350048fc8314dcfc8e65dd15a78d7b0
Lolymoly <teetat.wis@gmail.com>
2022-07-03 03:43:00 +0700

    fix user-not-found bug

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,12 +1,14 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 
-module.exports.checkLogin = function (req, res, next) {
+module.exports.checkLogin = async function (req, res, next) {
     const token = req.header('auth-token')
     if(!token) return res.status(401).send("Please login")
 
     try {
         const verified = jwt.verify(token, process.env.TOKEN_SECRET)
+        const thisUser = await User.findOne({_id: verified._id})
+        if(!thisUser) return res.status(400).send("Invalid Token")
         req.user = verified;
         next()
     } catch (err) {
@@ -19,7 +21,6 @@ module.exports.checkAdmin = async function (req, res, next) {
         const userID = req.user._id;
         const thisUser = await User.findOne({_id: userID});
         const userRole = thisUser.role
-
         if(userRole != "admin") return res.status(403).send("Permission required")
         next()
     } catch (err) {
